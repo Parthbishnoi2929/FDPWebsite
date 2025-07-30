571fb4ca76dd54357102ce82bae4467a4237e971
parthbishnoi2929 <parthbishnoi1609@gmail.com>
2025-07-30 13:49:45 +0530

    deploy now

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { ApprovalsDashboard } from "@/pages/ApprovalsDashboard";
 import { AllSubmissions } from "@/pages/AllSubmissions";
 import { UserManagement } from "@/pages/UserManagement";
 import { Reports } from "@/pages/Reports";
-import { Settings } from "@/pages/Settings";
+import Settings from "@/pages/Settings";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient({
